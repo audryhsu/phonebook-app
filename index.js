734e951f9e07ab527abca7a426c08f7755365328
Audry Hsu <audry.hsu@gmail.com>
2022-03-22 15:30:48 -0400

    fixed error handling notifications

--- index.js
+++ index.js
@@ -101,6 +101,8 @@ app.post('/api/persons', (req,res) => {
 
 })
 
+app.use(unknownEndpoint)
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log('Listening on port' + PORT);
